e3a1afae0851e9ad384b0278e079fadbd9a3bd84
Mukarram Abdulkarim <mukyalbani1@gmail.com>
2024-09-10 15:18:27 +0100

    Adding guards to the product

--- src/order/order.controller.ts
+++ src/order/order.controller.ts
@@ -1,13 +1,13 @@
 import { Controller, Post, Body, Req, UseGuards } from '@nestjs/common';
 import { OrderService } from './order.service';
 import { CreateOrderDto } from './dto/create-order.dto';
-import { AuthGuard } from 'src/auth/auth.guard';
+import { JwtGuard } from 'src/guards/jwt.guard';
 
 @Controller('orders')
 export class OrderController {
   constructor(private readonly orderService: OrderService) {}
 
-  @UseGuards(AuthGuard)
+  @UseGuards(JwtGuard)
   @Post()
   async createOrder(@Body() createOrderDto: CreateOrderDto, @Req() req): Promise<any> {
     const user = req.user; // The authenticated user
--- src/order/order.module.ts
+++ src/order/order.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { OrderService } from './order.service';
 import { OrderController } from './order.controller';
+import { User } from 'src/user/entities/user.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports : [TypeOrmModule.forFeature([User])],
   providers: [OrderService],
   controllers: [OrderController]
 })
