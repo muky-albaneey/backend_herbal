30f682a3ede5781b66c6f414c59f9e1613e9683b
Mukarram Abdulkarim <mukyalbani1@gmail.com>
2024-09-10 13:40:31 +0100

    Adding guards to the product

--- src/guards/jwt.guard.ts
+++ src/guards/jwt.guard.ts
@@ -5,45 +5,8 @@ import { Request } from "express";
 import { Observable } from 'rxjs';
 import { ConfigService } from '@nestjs/config';
 
-// @Injectable()
-// export class JwtGuard implements CanActivate {
-
-//     constructor(private readonly jwt: JwtService, private readonly configService: ConfigService) {}
-
-//     async canActivate(context: ExecutionContext): Promise<boolean> {
-//         const request = context.switchToHttp().getRequest();
-        
-
-        
-//         const token = this.extractTokenFromCookies(request);
-//         console.log('Before setting user property:', request);
-//         if (!token) {
-//             throw new UnauthorizedException();
-//         }
-
-//         try {
-//             const payload = await this.jwt.verifyAsync(token, {
-//                 secret: this.configService.get<string>('ROLE_TOKEN'),  
-//             });
-
-//             request['user'] = payload;
-
-//         } catch {
-//             throw new UnauthorizedException();
-//         }
-
-//         console.log('After setting user property:', request);
-
-//         return true;
-//     }
-
-//     private extractTokenFromCookies(req: Request) {
-//         return req.cookies?.roleToken;
-//     }
-// }
-
 @Injectable()
-export class     implements CanActivate {
+export class JwtGuard implements CanActivate {
 
     constructor(private readonly jwt: JwtService, private readonly configService: ConfigService,) {}
 
