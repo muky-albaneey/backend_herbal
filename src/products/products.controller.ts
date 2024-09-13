81bda49f4da0c372db74623c6c81388be7c932b4
Mukarram Abdulkarim <mukyalbani1@gmail.com>
2024-09-13 09:16:19 +0100

    Adding guards to the product

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -6,19 +6,13 @@ import {
   UseInterceptors,
   BadRequestException,
   ParseUUIDPipe,
+  Get,
 } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
 import * as path from 'path';
 import { ProductService } from './products.service';
 import { CreateProductDto } from 'src/user/dto/create-product.dto';
 import { UserService } from 'src/user/user.service';
-
-
-// import { Controller, Post, Body, UploadedFile, UseInterceptors, BadRequestException } from '@nestjs/common';
-// import { FileInterceptor } from '@nestjs/platform-express';
-// import { ProductService } from './product.service';
-// import { CreateProductDto } from './create-product.dto';
-// import { UserService } from '../users/user.service';
 
 @Controller('products')
 export class ProductController {
@@ -56,68 +50,11 @@ export class ProductController {
     // Call the service to create the product and link it to the user
     return await this.productService.createProductWithImage(createProductDto, file, user);
   }
+
+
+  @Get('all')
+  async findAll() {
+      return await this.productService.findAllProducts();
+    }
 }
 
-
-// @Controller('products')
-// export class ProductController {
-//   constructor(private readonly productService: ProductService) {}
-
-//   @Post()
-//   @UseInterceptors(
-//     FileInterceptor('file', {
-//       fileFilter: (req, file, callback) => {
-//         const ext = path.extname(file.originalname).toLowerCase();
-//         if (!['.jpeg', '.jpg', '.png', '.gif'].includes(ext)) {
-//           return callback(new BadRequestException('Invalid image file format'), false);
-//         }
-//         callback(null, true);
-//       },
-//     }),
-//   )
-//   async createProduct(
-//     @UploadedFile() file: Express.Multer.File, // Handling the uploaded file
-//     @Body() createProductDto: CreateProductDto, // The rest of the product details
-//   ) {
-//     if (!file) {
-//       throw new BadRequestException('Image file is required');
-//     }
-
-//     // Pass the product details and file to the service
-//     return await this.productService.createProductWithImage(createProductDto, file);
-//   }
-// }
-// @Controller('products')
-// export class ProductController {
-//   constructor(private readonly productService: ProductService) {}
-//   @Post()
-//   @UseInterceptors(
-//     FileInterceptor('file', {
-//       fileFilter: (req, file, callback) => {
-//         const ext = path.extname(file.originalname).toLowerCase();
-//         if (!['.jpeg', '.jpg', '.png', '.gif'].includes(ext)) {
-//           return callback(new BadRequestException('Invalid image file format'), false);
-//         }
-//         callback(null, true);
-//       },
-//     }),
-//   )
-//   async createProduct(
-//     @UploadedFile() file: Express.Multer.File,
-//     @Body() createProductDto: CreateProductDto,
-//   ) {
-//     if (!file) {
-//       throw new BadRequestException('Image file is required');
-//     }
-  
-//     // Use the userId from the form (createProductDto) to find the user
-//     const user = await this.productService.findById(createProductDto.userId);
-//     if (!user) {
-//       throw new BadRequestException('User not found');
-//     }
-  
-//     // Pass both the product data and the user to the service
-//     return await this.productService.createProductWithImage(createProductDto, file, user);
-//   }
-  
-// }
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -2,23 +2,12 @@
 import { Injectable, BadRequestException, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
-// import { Product } from './product.entity';
-// import { ProductImage } from './product_img.entity';
 import * as path from 'path';
 import { Product } from 'src/user/entities/product.entity';
 import { ProductImage } from 'src/user/entities/product_img.entity';
 import { CreateProductDto } from 'src/user/dto/create-product.dto';
 import { User } from 'src/user/entities/user.entity';
 
-
-
-// import { Injectable } from '@nestjs/common';
-// import { InjectRepository } from '@nestjs/typeorm';
-// import { Repository } from 'typeorm';
-// import { Product } from './product.entity';
-// import { CreateProductDto } from './create-product.dto';
-// import { User } from '../users/user.entity';
-// import { ProductImage } from './product-image.entity';
 
 @Injectable()
 export class ProductService {
@@ -53,129 +42,10 @@ export class ProductService {
 
     return await this.productRepository.save(newProduct);
   }
+
+  async findAllProducts() {
+    const product = await this.productRepository.find({      
+      relations: { product_image: true}     });
+    return product
+  }
 }
-
-// @Injectable()
-// export class ProductService {
-//   constructor(
-//     @InjectRepository(Product)
-//     private readonly productRepository: Repository<Product>,
-//     @InjectRepository(ProductImage)
-//     private readonly productImageRepository: Repository<ProductImage>,
-//   ) {}
-
-//   async createProductWithImage(createProductDto: any, file: Express.Multer.File) {
-//     // Validate the image format
-//     const validImageFormats = ['.jpeg', '.png', '.gif', '.jpg'];
-//     const ext = path.extname(file.originalname).toLowerCase();
-//     if (!validImageFormats.includes(ext)) {
-//       throw new BadRequestException('Invalid image file format');
-//     }
-
-//     // Convert the image buffer to base64
-//     const base64Image = file.buffer.toString('base64');
-
-//     // Create the ProductImage entity
-//     const productImage = this.productImageRepository.create({
-//       name: file.originalname,
-//       base64: base64Image,
-//       ext: ext.slice(1), // Store the file extension (without the dot)
-//       content: file.buffer,
-//     });
-
-//     // Save the ProductImage to the database
-//     const savedProductImage = await this.productImageRepository.save(productImage);
-
-//     // Create the Product entity with the reference to ProductImage
-//     const newProduct = this.productRepository.create({
-//       ...createProductDto, // Spread the product details
-//       product_image: savedProductImage, // Link to the ProductImage entity
-//     });
-
-//     // Save the Product to the database
-//     return await this.productRepository.save(newProduct);
-//   }
-// }
-// @Injectable()
-// export class ProductService {
-//   constructor(
-//     @InjectRepository(Product)
-//     private readonly productRepository: Repository<Product>,
-//     @InjectRepository(ProductImage)
-//     private readonly productImageRepository: Repository<ProductImage>,
-//     @InjectRepository(User)
-//     private readonly userRepository: Repository<User>,  // Inject UserRepository
-//   ) {}
-
-//   async createProductWithImage(
-//     createProductDto: CreateProductDto,
-//     file: Express.Multer.File,
-//     user: User,  // User entity passed from the controller
-//   ) {
-//     const ext = path.extname(file.originalname).toLowerCase();
-//     const base64Image = file.buffer.toString('base64');
-  
-//     const productImage = this.productImageRepository.create({
-//       name: file.originalname,
-//       base64: base64Image,
-//       ext: ext.slice(1),
-//       content: file.buffer,
-//     });
-  
-//     const savedProductImage = await this.productImageRepository.save(productImage);
-  
-//     // Create the product with the reference to the user
-//     const newProduct = this.productRepository.create({
-//       ...createProductDto,
-//       product_image: savedProductImage,
-//       user,  // Associate the product with the user
-//     });
-  
-//     return await this.productRepository.save(newProduct);
-//   }
-  
-  // async createProductWithImage(createProductDto: CreateProductDto, file: Express.Multer.File) {
-  //   const { userId, name, price, quantity, category, description } = createProductDto;
-
-  //   // Fetch the user by userId
-  //   const user = await this.userRepository.findOne({ where: { id: userId } });
-  //   if (!user) {
-  //     throw new BadRequestException('User not found');
-  //   }
-
-  //   // Validate the image format
-  //   const ext = path.extname(file.originalname).toLowerCase();
-  //   const validImageFormats = ['.jpeg', '.png', '.gif', '.jpg'];
-  //   if (!validImageFormats.includes(ext)) {
-  //     throw new BadRequestException('Invalid image file format');
-  //   }
-
-  //   // Convert the image buffer to base64
-  //   const base64Image = file.buffer.toString('base64');
-
-  //   // Create the ProductImage entity
-  //   const productImage = this.productImageRepository.create({
-  //     name: file.originalname,
-  //     base64: base64Image,
-  //     ext: ext.slice(1),
-  //     content: file.buffer,
-  //   });
-
-  //   // Save the ProductImage entity
-  //   const savedProductImage = await this.productImageRepository.save(productImage);
-
-  //   // Create the Product entity and link to the user
-  //   const newProduct = this.productRepository.create({
-  //     name,
-  //     price,
-  //     quantity,
-  //     category,
-  //     description,
-  //     product_image: savedProductImage,
-  //     user,  // Associate the user with the product
-  //   });
-
-  //   // Save the Product to the database
-  //   return await this.productRepository.save(newProduct);
-  // }
-// }
