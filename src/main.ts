d1088a04210f53c5373b03eca890cca844f62549
Mukarram Abdulkarim <mukyalbani1@gmail.com>
2024-09-16 08:52:54 +0100

    making changes to user service

--- src/main.ts
+++ src/main.ts
@@ -4,32 +4,39 @@
 // import { ValidationPipe } from '@nestjs/common';
 // import { ConfigService } from '@nestjs/config';
 // import * as dotenv from 'dotenv';
-// async function bootstrap() {
+// import * as express from 'express';
 
-//     dotenv.config();
+// async function bootstrap() {
+//   dotenv.config();
 //   const app = await NestFactory.create(AppModule);
+
+//   // Middleware to parse raw body for Stripe webhooks
+//   app.use('/paystack/webhook', express.raw({ type: 'application/json' }));
+
+//   // Default JSON body parser for other routes
+//   app.use(express.json());
+
+//   app.use(cookieParser());
+
 //   app.enableCors({
 //     origin: true,
 //     credentials: true,
-//     methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'], // Include 'PATCH' in the allowed methods
+//     methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
 //   });
-//   app.use(cookieParser());
 
 //   app.useGlobalPipes(new ValidationPipe({
-//     whitelist : true,
+//     whitelist: true,
 //     forbidNonWhitelisted: true,
 //     transform: true,
 //   }));
 
 //   const configService = app.get(ConfigService);
-  
 //   const PORT = configService.get<number>('PORT') || 3000;
 //   await app.listen(PORT, '0.0.0.0', () => {
 //     console.log(`Running in ${configService.get<string>('NODE_ENV')} on port ${PORT}`);
 //   });
 // }
 // bootstrap();
-// src/main.ts
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
@@ -50,8 +57,11 @@ async function bootstrap() {
 
   app.use(cookieParser());
 
+  // Get the frontend URL from environment variables
+  const frontendUrl = process.env.FRONTEND_URL || 'https://kenzy-dashboard.onrender.com';
+
   app.enableCors({
-    origin: true,
+    origin: frontendUrl,
     credentials: true,
     methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
   });
--- src/user/entities/product.entity.ts
+++ src/user/entities/product.entity.ts
@@ -1,42 +1,8 @@
-// import { OneToOne, JoinColumn, Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
-// import { ProductImage } from './product_img.entity';
-
-
-// @Entity()
-// export class Product{
-//     @PrimaryGeneratedColumn("uuid")
-//     id: number;
-
-//     @Column({nullable : false, length: 190, type: 'varchar'})
-//     name: string;
-
-//     @Column({ type: 'varchar', length: 190, nullable: false })
-//     price: string;
-
-//     @Column({ type: 'varchar', length: 190, nullable: false  })
-//     quantity: string;
-
-//     @Column({ type: 'varchar', length: 190, nullable: false  })
-//     category : string;
-
-//     @Column({ type: 'varchar', length: 190, nullable: false  })
-//     description : string;
-
-//     @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
-//     createdAt: Date;
-
-//     @OneToOne(() => ProductImage, { cascade: true , nullable: true})
-//     @JoinColumn()
-//     product_image?: ProductImage;
-
-//     constructor(product :Partial<Product>){
-//         Object.assign(this, product)
-//     }
-// }   
-/* eslint-disable prettier/prettier */
+  /* eslint-disable prettier/prettier */
 import { Entity, PrimaryGeneratedColumn, Column, OneToOne, JoinColumn, ManyToOne } from 'typeorm';
 import { ProductImage } from './product_img.entity';
 import { User } from './user.entity';
+
 
 @Entity()
 export class Product {
@@ -66,8 +32,9 @@ export class Product {
     product_image?: ProductImage;
 
     @ManyToOne(() => User, user => user.products)
-    user: User;  // Many products can belong to one user
+    user: User;  
 
+    
     constructor(product: Partial<Product>) {
         Object.assign(this, product);
     }
