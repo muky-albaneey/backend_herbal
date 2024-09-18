a6b32082bd1d02980456cf4416bede7b77693205
Mukarram Abdulkarim <mukyalbani1@gmail.com>
2024-09-18 10:10:09 +0100

    making changes to user service

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -23,7 +23,7 @@ export class CreateAuthDto {
    
    
     @IsNotEmpty({ message: "The password field is empty" })
-    // @MinLength(6, { message: "The password should exceed 5 characters" })
+    @MinLength(6, { message: "The password should exceed 5 characters" })
     @MaxLength(14, { message: "The password should not exceed 14 characters" })     
     @IsString()  
     password: string;
