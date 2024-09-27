b717908959945ed155c92afc16ae55c85d8c3bb2
Mukarram Abdulkarim <mukyalbani1@gmail.com>
2024-09-27 07:39:58 +0100

    removing trailing slash

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -33,36 +33,6 @@ export class ProductController {
   ) {}
 
   // @UseGuards(JwtGuard)
-  // @Post()
-  // @UseInterceptors(
-  //   FileInterceptor('file', {
-  //     fileFilter: (req, file, callback) => {
-  //       const ext = path.extname(file.originalname).toLowerCase();
-  //       if (!['.jpeg', '.jpg', '.png', '.gif'].includes(ext)) {
-  //         return callback(new BadRequestException('Invalid image file format'), false);
-  //       }
-  //       callback(null, true);
-  //     },
-  //   }),
-  // )
-  // async createProduct(
-  //   @UploadedFile() file: Express.Multer.File,
-  //   @Body() createProductDto: CreateProductDto,
-  //   // @Req() req: Request // Access the request object
-  // ) {
-  //   if (!file) {
-  //     throw new BadRequestException('Image file is required');
-  //   }
- 
-  //   const userId = createProductDto.userId // Access the userId from the request
-  //   const user = await this.userService.findOne(userId);
-  //   if (!user) {
-  //     throw new BadRequestException('User not found');
-  //   }
-
-  //   // Call the service to create the product and link it to the user
-  //   return await this.productService.createProductWithImage(createProductDto, file, user);
-  // }
   @Post()
   @UseInterceptors(
     FileInterceptor('file', {
