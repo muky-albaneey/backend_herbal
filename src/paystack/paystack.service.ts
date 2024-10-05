df0612a3808e16c192d6d4bf4a5ac13f0bb4d014
Mukarram Abdulkarim <mukyalbani1@gmail.com>
2024-10-05 12:34:33 +0100

    adding user to the product

--- src/paystack/paystack.service.ts
+++ src/paystack/paystack.service.ts
@@ -11,7 +11,7 @@ async initializePayment(email: string, amount: number, currency: string, callbac
     amount: this.convertAmount(amount, currency), // Convert amount to the smallest unit
     currency: currency.toUpperCase(), // Ensure currency is uppercase (e.g., USD, GHS)
     callback_url: callback_url, // Correctly include the callback URL here
-    metadata: { message: message } // Include the message in metadata
+    // metadata: { message: message } // Include the message in metadata
   });
 
   const options = {
